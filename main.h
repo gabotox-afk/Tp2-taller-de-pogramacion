9b5ccf7b537202dd4fb0d3c426be70ff2101af9f
FranReyes3223 <francucho999@gmail.com>
2025-09-24 09:59:22 -0300

    cambio nombres

--- main.h
+++ main.h
@@ -4,13 +4,13 @@
 #include "blockfederada.h"
 #include "arbol_validacion.h"
 
-void alta(int id_b , _blockFederada* bf ,char* mensaje, int* c,  int* p);
+void alta(int id_b , _blockFederada* bf ,char* mensaje, int* contador_primos,  int* primos);
 
-void actualizacion(_blockFederada* bf, int id_b, int id_n, char* nm, int* c,  int* p);
+void actualizacion(_blockFederada* bf, int id_block, int id_nodo, char* nuevo_mensaje, int* contador_primos,  int* primos);
 
 int validacion(_blockFederada* bf);
 
-int validacion_sub(int vp, int min, int max, _blockFederada bf);
+int validacion_sub(int validacion_esperada, int min, int max, _blockFederada bf);
 
 
 #endif
