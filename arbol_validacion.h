da0a67c19065684e6373a305263851a401516de7
FranReyes3223 <francucho999@gmail.com>
2025-09-24 19:46:01 -0300

    arbol mejorado e implementado

--- arbol_validacion.h
+++ arbol_validacion.h
@@ -1,49 +1,48 @@
-#ifndef ARBOL_VALIDACION_H
-#define ARBOL_VALIDACION_H
-#include "blockchain.h"
-
-typedef struct Arbol_v {
-    int* datos;
-    int cantidad_nodos;
-    int capacidad;
+#ifndef arbol_validacion_H
+#define arbol_validacion_H
+#include "blockfederada.h"
+// La estructura que define el nuevo modelo de árbol.
+typedef struct arbol_v {
+    int raiz_valor;
+    int* hojas;
+    int cantidad_hojas;
+    int capacidad_hojas;
 } arbol_v;
 
 
+/**
+ * Crea un contenedor vacío para el árbol simplificado.
+ */
+arbol_v* crear_arbol_validacion(int capacidad_inicial);
 
-void imprimir_arbol(arbol_v* arbol);
+/**
+ * Libera toda la memoria del árbol simplificado.
+ */
+void liberar_arbol_validacion(arbol_v* arbol);
 
-//expande la capacidad del arbol
-void expandir_capacidad( arbol_v* arbol);
+/**
+ * Función principal para actualizar o agregar una hoja.
+ * Después de cambiar el valor de una hoja, recalcula la raíz.
+ */
+void actualizar_hoja(arbol_v* arbol, int indice_hoja, int nuevo_valor);
 
-//crea el arbol de validacion
-arbol_v* crear_arbol_validacion(int largo_inicial);
+/**
+ * (Para validacion_total) Devuelve el valor cacheado de la raíz.
+ */
+int obtener_valor_raiz(arbol_v* arbol);
 
-//libera la memoria del arbol de validacion
-void liberar_arbol_validacion(arbol_v* red);
+/**
+ * (Para validacion_subconjunto) Calcula el valor para un rango de hojas.
+ */
+int calcular_valor_subconjunto(arbol_v* arbol, int inicio, int fin);
 
-//calcula la capacidad de hojas necesaria, con la cantidad de hojas
-int calcular_capacidad_hojas(int numero_de_blocks);
-
-//obtiene el indice de la hoja de la blockchain, con su indice y la capacida de hojas
-int obtener_indice_hoja(int capacidad_hojas, int indice_blockchain);
-
-//
-int obtener_indice_padre(int indice_hijo);
-
-//construye el arbol
-arbol_v* construir_arbol(int* ids_hojas, int cantidad_hojas);
-
-void calcular_padres(arbol_v* arbol, int indice);
-
-
-
-
-
-
-
+/**
+ * Imprime el estado del árbol simplificado para depuración.
+ */
+void imprimir_arbol_validacion(arbol_v* arbol);
 
 
-#endif
+#endif 
 
 
 
