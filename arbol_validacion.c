fabf4b0ac11db83eb3665fa43198c8c87dc48ca3
FranReyes3223 <francucho999@gmail.com>
2025-09-23 21:11:29 -0300

    arbol verificado

--- arbol_validacion.c
+++ arbol_validacion.c
@@ -15,18 +15,21 @@ void expandir_capacidad(arbol_v* arbol) {
 arbol_v* crear_arbol_validacion(int largo_inicial){
     arbol_v *nuevo = (arbol_v*) malloc (sizeof(arbol_v));
 
-    int cont = 1;
-    while(cont < largo_inicial){
-        cont *= 2;
+    int capacidad_nueva = 1;
+    while(capacidad_nueva < largo_inicial){
+        capacidad_nueva *= 2;
     }
-
-    nuevo->len= largo_inicial;
-    nuevo->datos= malloc (largo_inicial*sizeof(int));
-    //nuevo -> cantidad_nodos= 0;
+    
+    capacidad_nueva *= 2;
+    nuevo->len= capacidad_nueva;
+    nuevo -> ultimo = 0;
+    nuevo->datos= malloc (capacidad_nueva*sizeof(int));
     return nuevo;
 }
 
 void liberar_arbol_validacion(arbol_v* arbol) {
+
+
 
     free(arbol->datos);
     free(arbol);
