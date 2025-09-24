92c043a6930ec1116af201d4a9dfb30266012217
FranReyes3223 <francucho999@gmail.com>
2025-09-24 09:25:46 -0300

    arbol validacion

--- arbol_validacion.c
+++ arbol_validacion.c
@@ -12,15 +12,12 @@ void expandir_capacidad(arbol_v* arbol) {
 
 }
 
-arbol_v* crear_arbol_validacion(int largo_inicial){
+arbol_v* crear_arbol_validacion(int numero_de_blocks){
     arbol_v *nuevo = (arbol_v*) malloc (sizeof(arbol_v));
 
-    int capacidad_nueva = 1;
-    while(capacidad_nueva < largo_inicial){
-        capacidad_nueva *= 2;
-    }
-    
-    capacidad_nueva *= 2;
+
+    int capacidad_nueva = calcular_capacidad_hojas(numero_de_blocks);
+
     nuevo->len= capacidad_nueva;
     nuevo -> ultimo = 0;
     nuevo->datos= malloc (capacidad_nueva*sizeof(int));
@@ -29,8 +26,30 @@ arbol_v* crear_arbol_validacion(int largo_inicial){
 
 void liberar_arbol_validacion(arbol_v* arbol) {
 
-
-
+    if(!arbol) return;
     free(arbol->datos);
     free(arbol);
+}
+
+int calcular_capacidad_hojas(int numero_de_blocks){
+    int capacidad_nueva = 1;
+    while(capacidad_nueva < numero_de_blocks){
+        capacidad_nueva *= 2;
+    }
+
+    capacidad_nueva *= 2;
+    return capacidad_nueva;
+
+}
+
+int obtener_indice_hoja(int capacidad_hojas, int indice_blockchain){
+
+    return capacidad_hojas + indice_blockchain;
+
+}
+
+int obtener_indice_padre(int indice_hijo){
+
+    return indice_hijo / 2;
+
 }
--- arbol_validacion.h
+++ arbol_validacion.h
@@ -13,6 +13,12 @@ arbol_v* crear_arbol_validacion(int largo_inicial);
 
 void liberar_arbol_validacion(arbol_v* red);
 
+int calcular_capacidad_hojas(int numero_de_hojas);
+
+int obtener_indice_hoja(int capacidad_hojas, int indice_blockchain);
+
+int obtener_indice_padre(int indice_hijo);
+
 
 #endif
 
